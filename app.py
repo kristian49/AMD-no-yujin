bba94e69d5902d33275da294f3bcf004ed322afe
kristian49 <kristianfransiscus48@gmail.com>
2024-06-07 21:30:09 +0700

    tambah

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def home():
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms = ['HS256'])
         user_info = db.users.find_one({'useremail': payload.get('id')})
-        return render_template("dashboard_lama.html", user_info = user_info)
+        return render_template("dashboard.html", user_info = user_info)
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return render_template('home.html')
 
@@ -117,7 +117,7 @@ def profile(account_name):
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms = ['HS256'])
         status = account_name == payload.get('id')
         user_info = db.users.find_one({'account_name': account_name}, {'_id': False})
-        return render_template('profile.html', user_info = user_info, status = status)
+        return render_template('profile_coba.html', user_info = user_info, status = status)
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for('home'))
 
